c36e013a45c186ffe0c6f98b747121693f3d94ff
Vitaliy <vitaxa17@yandex.ru>
2019-11-06 19:41:35 +0200

    initial WinnerController

---
+++ src/io/hexlet/xo/controllers/WinnerController.java
@@ -0,0 +1,12 @@
+package io.hexlet.xo.controllers;
+
+import io.hexlet.xo.model.Field;
+import io.hexlet.xo.model.Figure;
+
+public class WinnerController {
+
+    public Figure getWinner(Field field){
+
+    }
+
+}
